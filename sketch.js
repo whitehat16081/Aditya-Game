65b60f6450a52993c380318b212d8590d138cb26
whitehat16081 <72308381+whitehat16081@users.noreply.github.com>
2022-01-03 00:31:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
-var score=0
-var BackgroundImage,Astronout,Earth;
-var AilienGroup,AilienImg
+var PLAY = 1;
+var END = 0;
+var gameState=PLAY;
+var life=3
+var BackgroundImage,Astronaut,Earth;
+var AlienImg
 var Fuel,Goldcoin,Life;
 var Asteriod,Reset1,Reset2,Shoot;
 var Blast;
-var life=3
-var gameState=1
 
 function preload() {
+  GameOver=loadImage("assets/GameOver.png")
+  AstronautShoot=loadSound("assets/shooting.wav")
+  GameOverSound=loadSound("assets/GameOversound.wav")
   BackgroundImg=loadImage("assets/background.jpg")
   AstronautImg=loadImage("assets/spaceship.png")
-  AilienImg=loadImage("assets/ailein.png")
+  AlienImg=loadImage("assets/ailein.png")
   EarthImg=loadImage("assets/earth.png")
   DestroyEarthImg=loadImage("assets/destroyearth.png")
   FuelImg=loadImage("assets/fuel.png")
@@ -22,36 +26,161 @@ function preload() {
   ShootImg=loadImage("assets/shoot.png")
   Shoot2Img=loadImage("assets/shoot2.png")
   BlastImg=loadImage("assets/blast.png")
+
 }
+
 
 function setup(){
   createCanvas(windowWidth,windowHeight)
+
+  
 
   Background=createSprite(windowWidth,windowHeight/2);
   Background.addImage(BackgroundImg)
   Background.velocityX=-3
 
+
   Earth=createSprite(100,100,50,50)
    Earth.addImage(EarthImg)
-   Earth.scale=1.5
+   Earth.scale=1
+
+   Life=createSprite(780,40,30,30)
+   Life.addImage(LifeImg)
+   Life.scale=0.3
+
+   gameOver=createSprite(800,320,100,100)
+  gameOver.addImage(GameOver)
+  //gameOver.scale=;
 
   Astronaut=createSprite(300,500, 50,50)
   Astronaut.addImage(AstronautImg)
+  Astronaut.debug=true;
+  Astronaut.setCollider("rectangle",-10,0,200,200)
 
-   Reset2=createSprite(1600,100,20,20)
+
+   Reset2=createSprite(1400,100,20,20)
    Reset2.addImage(Reset2Img)
    Reset2.scale=0.5
+   Reset2.debug=true;
+   
 
-
-
+   AlienGroup=new Group();
+  AsteriodGroup=new Group();
+  //Asteriod2Group=new Group()
+  bulletGroup=new Group();
+  shootGroup=new Group();
 }
 
 function draw(){
-//background(BackgroundImg)
+drawSprites();
+
+textSize(40);
+fill("red")
+stroke("yellow")
+textFont("shadowed black")
+strokeWeight(5)
+text("  : "+ life, 800,50);
 
 if (Background.x <0){
   Background.x = Background.width/2;
 }
+
+if(gameState===PLAY){
+  gameOver.visible=false;
+  Reset2.visible=false;
+  //gameOver.visible = false;
+  AlienAttack();
+Asteriod();
+//Asteriod2();
+//AstronautFuel();
+for(var i=0;i<AlienGroup.length;i++){ 
+  if(AlienGroup.get(i).collide(Astronaut)){ 
+    AlienGroup.get(i).destroy();
+ GameOverSound.play()
+ life=life-1
+  }
+  }
+
+
+  
+for(var i=0;i<bulletGroup.length;i++){ 
+  if(bulletGroup.get(i).collide(Astronaut)){ 
+    bulletGroup.get(i).destroy();
+  GameOverSound.play()
+  life=life-1
+  }
+  }
+
+  /*for(var i=0;i<bulletGroup.length;i++){ 
+    if(bulletGroup.get(i).collide(Earth)){ 
+      bulletGroup.get(i).destroy();
+    GameOverSound.play()
+    life=life-1
+
+    }
+    }*/
+
+    
+  /*for(var i=0;i<AlienGroup.length;i++){ 
+    if(AlienGroup.get(i).collide(Earth)){ 
+      AlienGroup.get(i).destroy();
+    GameOverSound.play()
+    life=life-1
+    }
+    }*/
+
+      
+ /* for(var i=0;i<AsteriodGroup.length;i++){
+     if(AsteriodGroup.get(i).collide(Earth)){ 
+       AsteriodGroup.get(i).destroy();
+    GameOverSound.play()
+   life=life-1
+   
+    }
+    }*/
+
+    /*for(var i=0;i<Asteriod2Group.length;i++){ 
+      if(Asteriod2Group.get(i).collide(Earth)){ 
+        Asteriod2Group.get(i).destroy();
+      GameOverSound.play()
+      life=life-1
+
+      }
+      }*/
+
+      /*for(var i=0;i<Asteriod2Group.length;i++){ 
+        if(Asteriod2Group.get(i).collide(Astronaut)){ 
+          Asteriod2Group.get(i).destroy();
+        GameOverSound.play()
+        life=life-1
+  
+        }
+        }*/
+
+  for(var i=0;i<AsteriodGroup.length;i++){ 
+    if(AsteriodGroup.get(i).collide(Astronaut)){ 
+      AsteriodGroup.get(i).destroy();
+    GameOverSound.play()
+    life=life-1
+    }
+    }
+
+    for(var i=0;i<shootGroup.length;i++){
+      if(shootGroup.get(i).isTouching(AlienGroup)){
+        AlienGroup.destroyEach();
+      
+     }
+    }
+
+    for(var i=0;i<shootGroup.length;i++){
+      if(shootGroup.get(i).isTouching(bulletGroup)){
+        bulletGroup.destroyEach();
+        shootGroup.get(i).destroy()
+      
+     }
+    }
+    
+
 
 if(keyDown(UP_ARROW)){
   Astronaut.y=Astronaut.y-10
@@ -69,38 +198,127 @@ if(keyDown(LEFT_ARROW)){
   Astronaut.x=Astronaut.x-10
 }
 
-AileinAttack();
+if(keyDown("space")) {
+  AstronautShoot.play();
+}
+
+
 
 if(keyDown("space")){
   Shoot();
 }
-drawSprites();
-}
 
-function Shoot(){
- shoot=createSprite(300,500,5,5) 
- shoot.x=Astronaut.x+50;
- shoot.y=Astronaut.y
- shoot.addImage(ShootImg)
- shoot.scale=0.1
- shoot.velocityX=10
-} 
-
-function AileinAttack(){
-  if(frameCount % 100 === 0) {
-   Ailien=createSprite(1700,500,50,50)
-   Ailien.y = Math.round(random(50,windowHeight-100));
-   Ailien.addImage(AilienImg)
-   Ailien.scale=0.6
-   Ailien.velocityX=-5;
-   Ailien.lifetime=900
-
-   bullet=createSprite(Ailien.x-20,Ailien.y-20,20,20);
-   bullet.addImage(Shoot2Img);
-   bullet.velocityX=-7;
-   bullet.scale=0.2;
+if(life===0){
+  gameState=END;
   }
 }
 
 
 
+
+
+
+else if(gameState===END){
+  Reset2.visible=true;
+  AsteriodGroup.setVelocityXEach(0);
+  AlienGroup.setVelocityXEach(0);
+  bulletGroup.setVelocityXEach(0)
+  //Asteriod2Group.setVelocityXEach(0);
+  Astronaut.velocityX=0;
+  Background.velocityX=0;
+  
+  gameOver.visible = true;
+
+  //DestroyEarth=createSprite(120,100,50,50)
+  //DestroyEarth.addImage(DestroyEarthImg)
+ //DestroyEarth.scale=1.8
+ Earth.addImage(DestroyEarthImg)
+
+ if(mousePressedOver(Reset2)) {
+  reset();
+}
+}
+}
+function reset(){
+  gameState=PLAY;
+  life=3;
+  AsteriodGroup.destroyEach();
+  AlienGroup.destroyEach();
+  bulletGroup.destroyEach();
+
+  //gameOver.visible=false;
+}
+function Shoot(){
+ shoot=createSprite(300,440,5,5) 
+ shoot.x=Astronaut.x+100;
+ shoot.debug=true;
+ shoot.y=Astronaut.y
+ shoot.addImage(ShootImg)
+ shoot.scale=0.110
+ shoot.velocityX=8
+ shootGroup.add(shoot)
+} 
+
+function AlienAttack(){
+  if(frameCount % 120 === 0) {
+   Alien=createSprite(1700,500,50,50)
+   Alien.y = Math.round(random(50,windowHeight-100));
+   Alien.addImage(AlienImg)
+   Alien.scale=0.5
+   Alien.debug=true;
+   Alien.setCollider("rectangle",0,0,500,200)
+   Alien.velocityX=-7;
+   Alien.lifetime=900
+   AlienGroup.add(Alien)
+
+   bullet=createSprite(Alien.x-20,Alien.y-20,20,20);
+   bullet.addImage(Shoot2Img);
+   bullet.debug=true;
+   bullet.setCollider("rectangle",0,0,100,100)
+   bullet.velocityX=-8;
+   bullet.scale=0.2;
+   bulletGroup.add(bullet)
+   AstronautShoot.play();
+  }
+}
+
+function Asteriod(){
+  if(frameCount%150===0){
+  asteriod=createSprite(1700,800,30,30)
+  asteriod.debug=true;
+  //asteriod.setCollider("rectangle",0,0,200,200)
+  asteriod.y= Math.round(random(50,windowHeight-100));
+  asteriod.addImage(AsteriodImg)
+  asteriod.scale=1.5
+  asteriod.velocityX=-7
+  asteriod.lifetime=900
+  AsteriodGroup.add(asteriod)
+  }
+}
+
+/*function Asteriod2(){
+  if(frameCount%300===0){
+  asteriod2=createSprite(1700,200,30,30)
+  asteriod2.y= Math.round(random(50,windowHeight-100));
+  asteriod2.addImage(AsteriodImg)
+  asteriod2.debug=true;
+  //asteriod2.setCollider("rectangle",0,0,100,100)
+  asteriod2.scale=1.5
+  asteriod2.velocityX=-5
+  asteriod2.lifetime=900
+  Asteriod2Group.add(asteriod2)
+
+  }
+}*/
+
+function AstronautFuel(){
+  if(frameCount%500===0){
+    fuel=createSprite(1700,400,40,40)
+    fuel.y= Math.round(random(50,windowHeight-100));
+    fuel.addImage(FuelImg)
+    fuel.scale=0.3
+    fuel.velocityX=-14
+  }
+}
+
+ 
